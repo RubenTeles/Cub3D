3af41f79c6bbf351ff9bf88d4671145cd60e24a8
RubenTeles <ruben-teles@hotmail.com>
2022-10-26 14:32:27 +0100

    criado uma estrutura engine

---
+++ src/engine/ft_new_engine.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_new_engine.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rteles <rteles@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/25 00:49:41 by rteles            #+#    #+#             */
+/*   Updated: 2022/10/25 11:31:03 by rteles           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ft_engine.h>
+#include <mlx.h>
+/*
+void	new_engine(void)
+{
+	engine().ptr = mlx_init();
+	
+	if (!engine().ptr)
+		return ;
+	(engine()).win = 0;//mlx_new_window(engine().ptr, 1920, 1080, "WOLF EAT PIG'S");
+	(engine()).game = NULL;
+	(engine()).max_x = NULL;
+	(engine()).max_y = NULL;
+	(engine()).check_point[0] = NULL;
+	(engine()).check_point[1] = NULL;
+}
+
+t_engine	engine(void)
+{
+	static t_engine	e;
+
+	return (e);
+}
+*/
