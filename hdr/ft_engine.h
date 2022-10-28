38ffdbc59c1703392e183eaf9cbd0a27e951ccfc
RubenTeles <ruben-teles@hotmail.com>
2022-10-29 00:22:57 +0100

    fixs

--- hdr/ft_engine.h
+++ hdr/ft_engine.h
@@ -6,7 +6,7 @@
 /*   By: rteles <rteles@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 00:34:27 by rteles            #+#    #+#             */
-/*   Updated: 2022/10/26 21:48:04 by rteles           ###   ########.fr       */
+/*   Updated: 2022/10/29 00:21:11 by rteles           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,14 @@
 
 typedef struct s_engine				t_engine;
 typedef struct s_canva				t_canva;
+
+typedef struct	s_img {
+	void	*img;
+	char	*relative_path;
+	int		img_width;
+	int		img_height;
+}				t_img;
+
 
 typedef struct	s_data {
 	void	*img;
@@ -50,6 +58,7 @@ t_engine	*engine(void);
 
 //canva
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color);
+int	get_pixel_color(t_data *data, int x, int y);
 
 //Keys
 int			key_hook_mode1(int keycode, void *all);
--- src/engine/ft_canvas.c
+++ src/engine/ft_canvas.c
@@ -6,7 +6,7 @@
 /*   By: rteles <rteles@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 21:21:56 by rteles            #+#    #+#             */
-/*   Updated: 2022/10/26 21:47:14 by rteles           ###   ########.fr       */
+/*   Updated: 2022/10/29 00:19:31 by rteles           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	char	*dst;
 
 	if (color == 0)
-		return (0);
+		return ;
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
 }
