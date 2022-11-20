0aa040134377549c911a2ef2d056dc8b3e4ca0ca
António Abranches Pinto <antoniomariabranches@gmail.com>
2022-11-20 13:33:10 +0000

    comment ft_sound

--- src/engine/ft_sound.c
+++ src/engine/ft_sound.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_sound.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rteles <rteles@student.42.fr>              +#+  +:+       +#+        */
+/*   By: amaria-m <amaria-m@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/20 02:06:47 by rteles            #+#    #+#             */
-/*   Updated: 2022/11/20 02:13:16 by rteles           ###   ########.fr       */
+/*   Updated: 2022/11/20 12:37:23 by amaria-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 
 void	ft_sound(char *sound)
 {
-	int pid;
-	int i;
+	// int pid;
+	// int i;
 
-	pid = fork();
-	/*if (pid == 0)
-		i = system(sound);*/
-	(void)i;    
+	// // pid = fork();
+	// // if (pid == 0)
+	// // 	i = system(sound);
+	// (void)i;    
+	if (sound)
+		return ;
 }
