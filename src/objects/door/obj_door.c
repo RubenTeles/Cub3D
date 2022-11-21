c789c30738dcb5ea58b40de7414a971bb6e575c5
RubenTeles <ruben-teles@hotmail.com>
2022-11-21 12:38:11 +0000

    door open and close

--- src/objects/door/obj_door.c
+++ src/objects/door/obj_door.c
@@ -6,7 +6,7 @@
 /*   By: rteles <rteles@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/20 14:58:17 by rteles            #+#    #+#             */
-/*   Updated: 2022/11/20 18:12:11 by rteles           ###   ########.fr       */
+/*   Updated: 2022/11/21 12:35:52 by rteles           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,7 @@ int ft_create_door(t_object *door)
 {
 	door->avatar = (canva())->sprite(DOOR);
 	door->sprite = (canva())->sprite(DOOR);
-	door->collision = 0;
+	door->collision = 1;
 	door->interation = 0.5;
-	printf("DOOR: x: %f y: %f\n", door->pos[X], door->pos[Y]);
-	//printf("interation: x: %f y: %f\n", door->pos[X], door->pos[Y]);
     return (1);
 }
